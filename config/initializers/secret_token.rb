ce42b7886abf1852b56d02743bc6ec4f4546db8b
Pavan <pt4189@gmail.com>
2013-09-14 14:07:18 +0530

    Added some basic conigurations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialAggregator::Application.config.secret_key_base = 'e6d3e8211feafaa6d4f60727e81413cd0ee7a5dd6d861d35f6398b37d2c195d8586520bbccb780ff4e8257cac098d332edc8fabb5c134472039736ca38118ff2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialAggregator::Application.config.session_store :cookie_store, key: '_social_aggregator_session'
