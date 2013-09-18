26ee7f20db3aeab3786861cc88d935c1972842a9
Pavana Teja T <teja1221@gmail.com>
2013-09-18 07:34:57 +0530

    Fixed spelling mistake for facebook

--- jobs/facebook_all_accounts_fetch_job.rb
+++ jobs/facebook_all_accounts_fetch_job.rb
@@ -4,7 +4,7 @@ class FacebookAllAccountsFetchJob
   class << self
     def perform
       Social.all.each do |social|
-        if social.facbook_profile.present? && social.facbook_profile.authorized?
+        if social.facebook_profile.present? && social.facebook_profile.authorized?
           Resque.enqueue(FacebookFeedJob, social.id)
         end
       end
